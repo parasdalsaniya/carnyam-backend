702d9a19e89672631e7851922dfac73e5a3b19f2
Paras Dalsaniya <parasdalsaniya8@gmail.com>
2024-01-06 12:23:57 +0530

    added socket set up

---
+++ routes/api/socket/socket.module.js
@@ -0,0 +1,12 @@
+const addDriverLiveLocation = async (data) => {
+    try {
+        console.log('data from addDriverLiveLocation: ', data);
+    } catch (error) {
+        console.log('Error in addDriverLiveLocation: ', error);
+    }
+};
+
+
+module.exports = {
+    addDriverLiveLocation
+}
