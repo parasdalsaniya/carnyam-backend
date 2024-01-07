faa692f617b8cfc925d419d09c49de3b8249598d
Paras Dalsaniya <parasdalsaniya8@gmail.com>
2024-01-07 16:07:41 +0530

    code refactor for socket

--- app.js
+++ app.js
@@ -3,42 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var nodemailer = require("nodemailer");
 const http = require('http');
-const { Server } = require("socket.io");
-
 var indexRouter = require("./routes/index");
 const { seedData } = require("./seed/seed");
-const { addDriverLiveLocation } = require("./routes/api/socket/socket.module");
-const { checkSocketAccessToken } = require("./routes/middleware");
+const { initSocket } = require("./routes/api/socket");
 var app = express();
 
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
-});
-
-io.use(async (socket, next) => {
-  try {
-    const authenticated = await checkSocketAccessToken(socket, next)
-    if (authenticated) {
-      return next();
-    }
-  } catch (error) {
-    return next(new Error(error.message));
-  }
-});
-
-io.on('connection', (socket) => {
-  console.log('User connected', socket.driver_id);
-
-  socket.on("driver-live-location", (data) => {
-    console.log('driver-live-location', socket.driver_id)
-    addDriverLiveLocation(data)
-  });
-});
+initSocket(server)
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
